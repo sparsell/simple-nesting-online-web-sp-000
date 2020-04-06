636d62b9266724ba1c60439af8bfdd493f171207
Sonja Parsell <hello@sparsell.com>
2020-04-06 18:03:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -119,22 +119,24 @@ def changing_alan
 
       end
 
-      
-def adding_to_dennis
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
 
-end
+      def adding_to_dennis
+      	programmer_hash =
+       		{
+              :grace_hopper => {
+                :known_for => "COBOL",
+                :languages => ["COBOL", "FORTRAN"]
+              },
+              :alan_kay => {
+                :known_for => "Object Orientation",
+                :languages => ["Smalltalk", "LISP"]
+              },
+              :dennis_ritchie => {
+                :known_for => "Unix",
+                :languages => ["C"]
+              }
+           }
+
+           programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+           return programmer_hash
+      end
