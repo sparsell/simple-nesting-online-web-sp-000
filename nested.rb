0480ef8e7c4b55aab6b2630fd70d81da476bb715
Sonja Parsell <hello@sparsell.com>
2020-04-06 17:34:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,7 +115,7 @@ def changing_alan
       programmer_hash[:alan_kay][:known_for] = alans_new_info
 
      #change what Alan Kay is :known_for to the value of the #alans_new_info variable.
-    
+
 
       end
 
