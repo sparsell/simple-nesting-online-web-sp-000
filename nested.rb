94b2764a73199ae970b2ddeee0a58dbea4129191
Sonja Parsell <hello@sparsell.com>
2020-04-06 17:34:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,12 +109,15 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+      }
 
+      alans_new_info = "GUI"
+      programmer_hash[:alan_kay][:known_for] = alans_new_info
 
-end
+     #change what Alan Kay is :known_for to the value of the #alans_new_info variable.
+    
+
+      end
 
 def adding_to_dennis
 	programmer_hash =
