3fd1ad1c8785ded5d5a9ce9dedf040f2ef6744e1
Sonja Parsell <hello@sparsell.com>
2020-04-06 15:19:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,6 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+     dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
