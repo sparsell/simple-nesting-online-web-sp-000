fe6934d1034867fe3c6c7efc47041569465db87f
Sonja Parsell <hello@sparsell.com>
2020-04-06 15:25:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,7 +61,7 @@ def dennis_ritchies_language
      }
 
      dennis_ritchies_language = programmer_hash[:dennis_ritchie] = "languages"
-     return dennis_ritchies_language
+     return dennis_ritchies_language.to_s
 end
 
 def adding_matz
