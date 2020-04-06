04e92c6c8e18a7744925b5ceb70d3f0ef5e5486d
Sonja Parsell <hello@sparsell.com>
2020-04-06 15:37:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
 
