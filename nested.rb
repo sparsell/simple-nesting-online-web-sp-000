94689967135a2c3a900cb4334bdb9e8025a5e83c
Sonja Parsell <hello@sparsell.com>
2020-04-06 15:20:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
         }
      }
 
-     dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages]
+     dennis_ritchies_language = programmer_hash[:dennis_ritchie]["languages"]
      return dennis_ritchies_language
 end
 
